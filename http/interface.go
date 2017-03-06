5cfcd5151a4251ba232c43bb3f7ec122f8ebf82c
Maxim Lanin <max@lanin.me>
2017-03-06 13:24:44 +0300

    Add comments

--- http/interface.go
+++ http/interface.go
@@ -4,6 +4,9 @@ import "github.com/lara-go/larago/http/responses"
 
 // ErrorsHandlerContract for every handler to resolve errors during http calls..
 type ErrorsHandlerContract interface {
+	// Report error to logger.
 	Report(err error)
+
+	// Render error to return to the client.
 	Render(request *Request, err error) responses.Response
 }
--- interface.go
+++ interface.go
@@ -7,19 +7,30 @@ type Bootstrapper func(application *Application) error
 
 // ServiceProvider interface.
 type ServiceProvider interface {
+	// Register service.
 	Register(application *Application)
 }
 
 // ConsoleCommand interface.
 type ConsoleCommand interface {
+	// GetCommand for the cli to register.
 	GetCommand() cli.Command
+
+	// Handle command.
 	Handle(args cli.Args) error
 }
 
 // Config interface.
 type Config interface {
+	// Env returns current environment name.
 	Env() string
+
+	// Debug returs debug mode state.
 	Debug() bool
+
+	// Get value from config using dot-notation.
 	Get(key string) interface{}
+
+	// Set value to config using dot-notation.
 	Set(key string, value interface{})
 }
